122a9c44be42e860c13f66ee2eb4194f24893e8f
Askhat Suleimenov <s.askhat.b@gmail.com>
2024-03-08 20:55:23 +0500

    adding reg form component

---
+++ src/components/RegistrationForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>Регистрация</h2>
+    <form @submit.prevent="register">
+      <div>
+        <label for="username">Имя пользователя:</label>
+        <input type="text" id="username" v-model="username" required>
+      </div>
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" required>
+      </div>
+      <div>
+        <label for="password">Пароль:</label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <div>
+        <button type="submit">Зарегистрироваться</button>
+      </div>
+    </form>
+    <p v-if="registered">Вы успешно зарегистрированы!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      registered: false
+    };
+  },
+  methods: {
+    register() {
+      // Здесь обычно происходила бы отправка данных на сервер
+      // Но для простоты этот пример просто устанавливает флаг зарегистрированного пользователя
+      this.registered = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>

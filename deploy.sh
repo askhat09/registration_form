bdf0df5d6fdd20306a1a9a121e923f2d42f5f224
Askhat Suleimenov <s.askhat.b@gmail.com>
2024-03-09 00:43:15 +0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<askhat09>/<registration_form>.git main:gh-pages
+git push -f git@github.com:askhat09/registration_form.git main:gh-pages
 
 cd -
